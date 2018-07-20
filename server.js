2387b428dbb60dd09a39eb960a32dad5f7426aef
dev@dev1 <dev@dev1.revteltech.com>
2018-07-20 14:09:58 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+
+var express = require('express');
+var bodyParser =  require('body-parser');
+
+var app = express();
+//app.use('/', express.static(__dirname + './'));
+
+
+app.use(bodyParser.json()); // add a middleware (so that express can parse request.body's json)
+
+app.get('/', function (req, res) {
+  es.send('Hello World!')
+});
+
+app.post('/', function(req, res) {
+	var jsonString = ''
+	req.on('data', function (data) {
+		jsonString += data;
+	});
+
+	req.on('end', function () {
+		console.log(jsonString);
+			
+		function showJsonString(s) {
+			var j = s.split('&');
+			var ret = '';
+
+			j.forEach(function(line) {
+				var _l = line.split('=');
+				ret += _l[0]+': '+_l[1]+'<br>'
+			});
+
+			return ret;                                                                                                         
+		}
+		
+		res.send(showJsonString(jsonString));
+	});
+    //res.send('Hello World!')
+    //res.redirect('http://www.google.com');
+});
+
+app.use(express.static('public'));
+
+app.listen(8295, function() { console.log('listening 8295 ...'); });
